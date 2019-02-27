788e6a2327781d4e63e01fa2c13f322b9aaa1edd
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-27 07:57:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each {|key, value|
+    key1 = key
+    value.each {|key, value|
+      key2 = key
+      value.each {|element|
+        if hash.has_key?(value)
+          hash[value][key1] = key2_
+      
+  
 end
