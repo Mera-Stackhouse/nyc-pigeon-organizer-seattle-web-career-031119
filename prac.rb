796d5c2855a1f52c4df19bf068970c0eaac7b0b1
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-27 08:14:55 +0000

    Automatically backed up by Learn

--- prac.rb
+++ prac.rb
@@ -23,14 +23,15 @@ def nyc_pigeon_organizer(data)
   hash = {}
   data.each {|key, value|
     key1 = key
-    
+    binding.pry
     value.each {|key, value|
       key2 = key
-      
+      binding.pry
       value.each {|element|
         if !hash.has_key?(element)
           hash[element] = {}
         end
+        binding.pry
         if !hash[element].has_key?(key1)
           hash[element][key1] = []
           hash[element][key1] << "#{key2}"
