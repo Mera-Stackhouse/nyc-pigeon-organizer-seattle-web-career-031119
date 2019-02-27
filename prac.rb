343485eba2ac787dde361f75d8ddb641f71d7ed8
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-27 08:07:46 +0000

    Automatically backed up by Learn

--- prac.rb
+++ prac.rb
@@ -16,3 +16,27 @@ data = {
           "City Hall" => ["Andrew"]
         }
       }
+      
+def nyc_pigeon_organizer(data)
+  hash = {}
+  data.each {|key, value|
+    key1 = key
+    value.each {|key, value|
+      key2 = key
+      value.each {|element|
+        if !hash.has_key?(value)
+          hash[value] = {}
+        end
+        if !hash[value].has_key?(key1)
+          hash[value][key1] = []
+          hash[value][key1] << "#{key2}"
+        else
+          hash[value][key1] << "#{key2}"
+        end
+      }
+    }
+  }
+  puts hash
+end
+
+nyc_pigeon_organizer(data)
