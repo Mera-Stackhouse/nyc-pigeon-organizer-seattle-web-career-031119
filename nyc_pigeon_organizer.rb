27632a40486abc2060852d9a711b639e1123a9f7
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-27 08:13:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,20 +2,22 @@ def nyc_pigeon_organizer(data)
   hash = {}
   data.each {|key, value|
     key1 = key
+    
     value.each {|key, value|
       key2 = key
+      
       value.each {|element|
-        if !hash.has_key?(value)
-          hash[value] = {}
+        if !hash.has_key?(element)
+          hash[element] = {}
         end
-        if !hash[value].has_key?(key1)
-          hash[value][key1] = []
-          hash[value][key1] << "#{key2}"
+        if !hash[element].has_key?(key1)
+          hash[element][key1] = []
+          hash[element][key1] << "#{key2}"
         else
-          hash[value][key1] << "#{key2}"
+          hash[element][key1] << "#{key2}"
         end
       }
     }
   }
-  hash
+  puts hash
 end
